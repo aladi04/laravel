8e4a4daadf7b0bcba25aaa77f3f7f6ec8d307048
alaEddineBenHssine <alabenhssine6@gmail.com>
2025-01-18 20:51:06 +0100

    Database update

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    "database" => [
+        "host" => "localhost",
+        "port" => 3306,
+        "dbname" => "abc",
+        "charset" => "utf8mb4"
+    ]
+    
+];
--- index.php
+++ index.php
@@ -2,4 +2,14 @@
 
 require "functions/function.php";
 
-require "router.php";
+//require "router.php";
+require "Database.php";
+
+$config = require "config.php";
+
+$db = new Database($config["database"]);
+$titles = $db->query("select * from `ala`")->fetchAll();
+
+foreach ($titles as $title){
+    echo "<li>" . $title["title"] . "</li>";
+}
