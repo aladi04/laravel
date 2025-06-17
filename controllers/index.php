084290893d9bc47f022cdbb9f1f9071d43b77089
alaEddineBenHssine <alabenhssine6@gmail.com>
2025-06-17 18:08:37 +0100

    sessions

--- controllers/index.php
+++ controllers/index.php
@@ -1,5 +1,7 @@
 <?php
 
+$_SESSION['name']='aladin';
+
 view("index.view.php", [
     'heading' => 'HOME',
 ]);
--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+
+session_start();
+
 define('BASE_PATH', __DIR__ . "/../");
 
 require BASE_PATH . "functions/function.php";
